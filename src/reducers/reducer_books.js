49f2c59af501ad11e0121c0435511c435c3877a9
ludwiglevo <ludwiglevo@gmail.com>
2018-02-20 11:10:07 +0800

    Update book names.

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,7 +1,7 @@
 export default function() {
   return [
     {title: 'What it is like to learn JavaScript in 2016'},
-    {title: 'Korean War: The Forgotten History'},
+    {title: 'Korean War: The Forgotten War'},
     {title: 'Higgs Boson and the Standard Model'}
   ]
 }
